1d32f1fb045b1ded2a1446a2c0dccf3b4c95686d
Gustavo <gustavo.raposo@fatec.sp.gov.br>
2024-01-12 17:39:27 -0300

    Seller entity class

--- src/principal/Program.java
+++ src/principal/Program.java
@@ -1,6 +1,9 @@
 package principal;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
@@ -8,6 +11,8 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
 		
+		Seller seller = new Seller(21, "Roger", "rogergudes@gmail.com", new Date(), 3000.0, obj);
 		
+		System.out.println(seller);
 	}
 }
