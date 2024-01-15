702efd64d02d0feca41f684def5b5e0e60e9c6d9
Gustavo <gustavo.raposo@fatec.sp.gov.br>
2024-01-15 17:07:23 -0300

    FindById Implementation

--- src/principal/Program.java
+++ src/principal/Program.java
@@ -1,21 +1,16 @@
 package principal;
 
-import java.util.Date;
-
 import model.dao.DaoFatory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21, "Roger", "rogergudes@gmail.com", new Date(), 3000.0, obj);
-		
+	
 		SellerDao sellerDao = DaoFatory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(7);
 		
 		
 		System.out.println(seller);
